c4ec6bb6266c0aebe12fbed9c575ff6081354c82
Maximilian Hantsch <mhantsch@gmail.com>
2024-07-26 16:46:32 +0000

    switching to kanata_gui.exe for Windows which supports a tray icon

--- kanata/kanata.bat
+++ kanata/kanata.bat
@@ -1,2 +1,2 @@
 @echo off
-wt --size 72,8 new-tab --title "Kanata" --tabColor #8F2200 c:\Utilities\kanata_winIOv2.exe -c c:\Utilities\colemax-maxtend.kbd -c C:\dev\maxtend\kanata\colemax-maxtend.kbd
+wt --size 72,8 new-tab --title "Kanata" --tabColor #8F2200 c:\Utilities\kanata_gui.exe -c c:\Utilities\colemax-maxtend.kbd -c C:\dev\maxtend\kanata\colemax-maxtend.kbd
